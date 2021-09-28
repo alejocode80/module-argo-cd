80f64e9f565a42b6b8e7e5ef95c9d91ee43a4c7b
Alejandro Cadavid <alejandrocadavid@Alejandros-iMac.local>
2021-09-27 20:15:08 -0400

    main file changed

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
-    command     = "aws"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", var.kubernetes_cluster_name]
   }
 }
 
@@ -14,8 +14,8 @@ provider "helm" {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
-      command     = "aws"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", var.kubernetes_cluster_name]
     }
   }
 }
